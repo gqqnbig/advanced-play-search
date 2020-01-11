cc0b983e92ebc0d1e36457af3df29aadf82d9a49
gqqnbig <gqqnb2005@gmail.com>
2020-01-10 18:11:17 -0800

    Scrape permissions
    Dynamically construct the post info
    
    add app url for test purpose.
    
    #2

--- src/Program.py
+++ src/Program.py
@@ -2,51 +2,81 @@
 
 import re
 import scrapy
+import sys
+import urllib.parse as urlParse
 
 from scrapy.crawler import CrawlerProcess
+from types import SimpleNamespace
+from json import loads as jsonLoads
 
 
 class AppInfoSpider(scrapy.Spider):
 	name = "brickset_spider"
 	start_urls = ['https://play.google.com/store/apps/details?hl=en&id=com.mojang.minecraftpe',
-				  'https://play.google.com/store/apps/details?hl=en&id=com.sega.sonic1px']
+				  'https://play.google.com/store/apps/details?hl=en&id=com.sega.sonic1px',
+				  'https://play.google.com/store/apps/details?id=com.tencent.mm']
 
 	def parse(self, response):
+		appId = urlParse.parse_qs(urlParse.urlparse(response.url).query)['id'][0]
+
+		appInfo = SimpleNamespace()
+
 		h1 = response.css("h1[itemprop=name]")
-		appName = h1.css("*::text").get()
+		appInfo.appName = h1.css("*::text").get()
 
 		parentBox = h1.xpath('../..')
-		inAppPurchases = parentBox.xpath("div[text()[contains(.,'Offers in-app purchases')]]").get() is not None
-		containsAds = parentBox.xpath("div[text()[contains(.,'Contains Ads')]]").get() is not None
+		appInfo.inAppPurchases = parentBox.xpath("div[text()[contains(.,'Offers in-app purchases')]]").get() is not None
+		appInfo.containsAds = parentBox.xpath("div[text()[contains(.,'Contains Ads')]]").get() is not None
 		try:
 			# the first match is the rating box.
 			ariaLabel = response.css('c-wiz div[aria-label][role=img]::attr(aria-label)').get()
-			rating = float(re.search(r'\d\.\d', ariaLabel)[0])
+			appInfo.rating = float(re.search(r'\d\.\d', ariaLabel)[0])
 		except:
-			rating = None
+			appInfo.rating = None
 
-		print(f'appName={appName},  rating={rating}, inAppPurchases={inAppPurchases}, containsAds={containsAds}')
-
-		r = scrapy.FormRequest(r'https://play.google.com/_/PlayStoreUi/data/batchexecute?rpcids=xdSrCf&f.sid=324256068901206895&hl=en',
-							headers={"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"},
-							formdata={'f.req':r'[[["xdSrCf","[[null,[\"com.sega.sonic1px\",7],[]]]",null,"1"]]]'},
-							callback=self.callback,
-							errback=self.errback)
+		r = scrapy.FormRequest(r'https://play.google.com/_/PlayStoreUi/data/batchexecute?rpcids=xdSrCf&hl=en',
+							   headers={"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"},
+							   formdata={'f.req': r'[[["xdSrCf","[[null,[\"' + appId + r'\",7],[]]]",null,"1"]]]'},
+							   # not use cb_kwargs because it's only passed to callback, no errback.
+							   meta={'appInfo': appInfo},
+							   callback=self.permissions_retrieved,
+							   errback=self.errback)
 		yield r
 
 	def errback(self, failure):
-		print(failure)
+		appInfo = failure.request.meta['appInfo']
+		print(f'appName={appInfo.appName},  rating={appInfo.rating}, inAppPurchases={appInfo.inAppPurchases}, containsAds={appInfo.containsAds}, permissions=Not available')
 
-	def callback(self, response):
-		print(response.text)
+	# print(failure)
 
+	def permissions_retrieved(self, response):
+		appInfo = response.meta['appInfo']
+		data = jsonLoads(response.text[response.text.index('\n') + 1:])
+		permissionData = jsonLoads(data[0][2])
 
+		permissions = []
 
+		# permissionData[0] is grouped permissions.
+		if len(permissionData) > 0:
+			for g in permissionData[0]:
+				for p in g[2]:
+					permissions.append(p[1])
 
+		# permissionData[1] is other permissions.
+		if len(permissionData) > 1:
+			for p in permissionData[1][0][2]:
+				permissions.append(p[1])
 
+		# permissionData[2] is miscellaneous permissions.
+		if len(permissionData) > 2:
+			for p in permissionData[2]:
+				permissions.append(p[1])
 
+		if len(permissionData) > 3:
+			print('Unknown data in permission block.\npermissionData={}'.format(permissionData), file=sys.stderr)
 
-
+		print(f'appName={appInfo.appName},  rating={appInfo.rating}, inAppPurchases={appInfo.inAppPurchases}, containsAds={appInfo.containsAds}')
+		print(f'permissions={permissions}')
 
 
 process = CrawlerProcess(settings={
