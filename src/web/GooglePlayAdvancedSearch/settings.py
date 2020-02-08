2723554b4c6dcdbeb05268c5c9f20a895590b8ea
QQ喵 <gqqnb2005@gmail.com>
2020-02-08 01:20:55 -0800

    Static files should be put in src/web/StaticFiles/

--- src/web/GooglePlayAdvancedSearch/settings.py
+++ src/web/GooglePlayAdvancedSearch/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "StaticFiles")
+]
 
 try:
 	# django will import settings twice on development environment
