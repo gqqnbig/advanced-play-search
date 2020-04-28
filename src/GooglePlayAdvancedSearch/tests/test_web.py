b0cf6a7efb7918ed910ec018ae7f78ea8c34fb0c
QQ喵 <gqqnb2005@gmail.com>
2020-04-28 01:06:03 -0700

    Add test ApiErrorNotLeaking
    Use selenium for headless testing

--- src/GooglePlayAdvancedSearch/tests/test_web.py
+++ src/GooglePlayAdvancedSearch/tests/test_web.py
@@ -1,6 +1,12 @@
 import os
 import pytest
+import re
 import requests
+import shutil
+import time
+
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
 import GooglePlayAdvancedSearch.tests.testUtils as testUtils
 
@@ -22,6 +28,38 @@ def test_websiteEmptyStart():
 		pytest.skip(str(e))
 
 	testUtils.startWebsite(callback_testResponse)
+
+
+def callback_testApiErrorNotLeaking(websiteUrl):
+	options = Options()
+	options.headless = True
+	with Firefox(options=options) as driver:
+		driver.get(websiteUrl)
+		# get function returns when the page is loaded. However, there are javascript events listening the onload event, and run after this.
+		# Therefore we have to wait additional time.
+		time.sleep(2)
+		# driver.save_screenshot('test_ApiErrorNotLeaking.png')
+		assert '&lt;/html&gt;' not in driver.page_source, 'Even if API returns 500 error code, the home page should not show the raw response.'
+
+
+def test_ApiErrorNotLeaking():
+	apiFilePath= os.path.join(testFolder, '../django/web/Api.py')
+	backup=apiFilePath+".bk"
+	shutil.copyfile(apiFilePath,backup)
+
+	try:
+		with open(apiFilePath, 'r+') as file:
+			data = file.read()
+			data= re.sub(r"(^def\s.+:[\r\n][\r\n]?)",r"\1\traise Exception()\n",data, flags=re.MULTILINE)
+			file.seek(0)
+			file.write(data)
+			file.truncate()
+
+		testUtils.startWebsite(callback_testApiErrorNotLeaking)
+	finally:
+		os.remove(apiFilePath)
+		shutil.move(backup,apiFilePath)
+
 
 
 # Allow the file to be run by itself, not in the pytest environment.
