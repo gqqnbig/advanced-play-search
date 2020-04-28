743b4e52607467a8d953e356a88a66be41870b67
QQ喵 <gqqnb2005@gmail.com>
2020-04-28 12:52:09 -0700

    Remove old search records Issue: #14

--- src/GooglePlayAdvancedSearch/maintenance/staleAppRemover.py
+++ src/GooglePlayAdvancedSearch/maintenance/staleAppRemover.py
@@ -11,15 +11,18 @@ import sqlite3
 # 			cursor.execute(f'alter table app drop column {GooglePlayAdvancedSearch.DBUtils.delimiteDBIdentifier(permission)}')
 # 			connection.commit()
 
+MAX_RECORD_DAYS = 14
 APP_FRESH_DAYS = 7
 
 if __name__ == "__main__":
 	connection = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/db.sqlite3'))
 	cursor = connection.cursor()
 	cursor.execute("delete from app where updateDate<date('now','-" + str(APP_FRESH_DAYS) + " days')")
-
-	removedCount = cursor.rowcount
 	print(f'removed {cursor.rowcount} apps cached since {APP_FRESH_DAYS} days ago.')
+
+	cursor.execute("delete from Search where [date]<date('now','-" + str(MAX_RECORD_DAYS) + " days')")
+	print(f'removed {cursor.rowcount} search records since {MAX_RECORD_DAYS} days ago.')
+
 	connection.commit()
 
 # We may want to remove unused permissions or categories.
