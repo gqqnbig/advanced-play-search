fd7f82d552846d4dee6a8a18fbeca3de5db58160
QQ喵 <gqqnb2005@gmail.com>
2020-05-29 22:58:32 -0700

    change to use lastTick

--- src/GooglePlayAdvancedSearch/django/StaticFiles/js/jumpIntoLightSpeed.js
+++ src/GooglePlayAdvancedSearch/django/StaticFiles/js/jumpIntoLightSpeed.js
@@ -13,12 +13,17 @@ settings.starLifeTime = settings.starLifeTime || 700;
 settings.canvasWidth = settings.canvasWidth || "100%";
 settings.canvasHeight = settings.canvasHeight || "500px";
 settings.velocity = settings.velocity || 1;
+settings.stopVelocity = settings.stopVelocity || 2;
+
 settings.startDuration = settings.startDuration || 2000;
+settings.stopDuration = settings.stopDuration || 2000;
 
 let targetCanvas;
 let canvasHeight;
 let canvasWidth;
 let desiredStarCount;
+let lastTick;
+let stopRequestTime;
 
 function timingQuad(timeFraction) {
 	return Math.pow(timeFraction, 5)
@@ -49,7 +54,7 @@ function spawnStar(birthDate, classes) {
 	}
 }
 
-function lightSpeed(canvas) {
+function enterLightSpeed(canvas) {
 	targetCanvas = canvas;
 	// canvas.width = window.innerWidth;
 	// canvas.height = 500;
@@ -77,11 +82,65 @@ function lightSpeed(canvas) {
 		spawnStar(startTime - getRandomInt(0, settings.starLifeTime * 4), ['initial']);
 	}
 	canvas.classList.add('starting');
+	lastTick = startTime;
 
 	window.requestAnimationFrame(draw);
 }
 
 function animateTrail(now, velocityProgress) {
+	let lines = targetCanvas.querySelectorAll('line');
+
+	let centerX = canvasWidth / 2;
+	let centerY = canvasHeight / 2;
+
+	let currentVelocity = velocityProgress * settings.velocity;
+
+	let diedStars = 0;
+	for (const line of lines) {
+		let birth = parseInt(line.getAttribute('birth'));
+		if (birth + settings.starLifeTime / velocityProgress < now) {
+			targetCanvas.removeChild(line);
+			diedStars++;
+		}
+		else {
+
+			let x1 = line.x1.animVal.value;
+			let y1 = line.y1.animVal.value;
+
+			let timeDiff = now - lastTick;
+			let x2 = line.x2.animVal.value;
+			let y2 = line.y2.animVal.value;
+			if (x1 === centerX) {
+				x2 = x1;
+				if (y1 > centerY)
+					y2 = y2 + timeDiff * currentVelocity;
+				else
+					y2 = y2 - timeDiff * currentVelocity;
+			}
+			else {
+				let op2 = Math.sqrt(Math.pow(Math.abs(x2 - centerX), 2) + Math.pow(Math.abs(y2 - centerY), 2));
+				let op3 = op2 + timeDiff * currentVelocity;
+
+				x2 = op3 * (x2 - centerX) / op2 + centerX;
+				y2 = op3 * (y2 - centerY) / op2 + centerY;
+			}
+
+			line.setAttribute('x2', x2);
+			line.setAttribute('y2', y2);
+		}
+	}
+
+	let addStarCount = 0;
+	let spawnProbability = Math.max(0.1, 1 - (lines.length - diedStars) / desiredStarCount);
+	addStarCount = spawnProbability * (desiredStarCount - (lines.length - diedStars));
+	for (let i = 0; i < addStarCount; i++)
+		spawnStar(now);
+
+
+	console.log(`diedStars=${diedStars}, new stars=${addStarCount}`);
+}
+
+function animateTrailStopping(now, velocityProgress) {
 	let lines = targetCanvas.querySelectorAll('line');
 
 	let centerX = canvasWidth / 2;
@@ -124,33 +183,40 @@ function animateTrail(now, velocityProgress) {
 		}
 	}
 
-	let addStarCount = 0;
-	let spawnProbability = Math.max(0.1, 1 - (lines.length - diedStars) / desiredStarCount);
-	if (Math.random() < spawnProbability) {
-		addStarCount = spawnProbability * (desiredStarCount - (lines.length - diedStars));
-		for (let i = 0; i < addStarCount; i++)
-			spawnStar(now);
-	}
-
-
 	console.log(`diedStars=${diedStars}, new stars=${addStarCount}`);
 }
 
+
 function draw() {
-	let d = performance.now() - startTime;
-	if (d < settings.startDuration) {
-		animateTrail(performance.now(), timingQuad(d / settings.startDuration) / timingQuad(1));
+	let now = performance.now();
+	if (targetCanvas.classList.contains('stopping')) {
+		let d = now - stopRequestTime;
+		animateTrailStopping(now, 1 - timingQuad(d / settings.stopDuration) / timingQuad(1))
 	}
 	else {
-		if (!targetCanvas.classList.contains('running'))
-			targetCanvas.classList.add('running');
-		animateTrail(performance.now(), 1);
+		let d = now - startTime;
+		if (d < settings.startDuration) {
+			animateTrail(now, timingQuad(d / settings.startDuration) / timingQuad(1));
+		}
+		else //if (d < 10000)
+		{
+			if (!targetCanvas.classList.contains('running'))
+				targetCanvas.classList.add('running');
+			animateTrail(now, 1);
+		}
+		lastTick = now;
+		window.requestAnimationFrame(draw);
 	}
-	window.requestAnimationFrame(draw);
 }
 
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function exitLightSpeed() {
+	targetCanvas.classList.add('stopping');
+	stopRequestTime = performance.now();
+
 }
